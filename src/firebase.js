a3c1fdec8a165300ec5059a6a308d5dfaf728933
Fabio Cataleta <fcataleta@gmail.com>
2021-06-08 16:09:59 +0100

    -Removed testing and logo files -Removed default formatting -Wrote new skeleton for app layout -Added Firebase code

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAb-P3NsjdGEyZji_IW9wqsEOxUG5knpHI",
+    authDomain: "whatsapp-mern-8fa7b.firebaseapp.com",
+    projectId: "whatsapp-mern-8fa7b",
+    storageBucket: "whatsapp-mern-8fa7b.appspot.com",
+    messagingSenderId: "231292758311",
+    appId: "1:231292758311:web:624d4b30aa879a9f8fe102",
+    measurementId: "G-M79L6DPEEG"
+  };
