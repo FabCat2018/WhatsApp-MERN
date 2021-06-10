fe7fc6be3e3167a3f0640fc7d2ed6b6850245ad8
Fabio Cataleta <fcataleta@gmail.com>
2021-06-10 12:28:05 +0100

    -Added Sidebar Search component -Styled Sidebar Search component -Fixed Sidebar Imports

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from 'react';
+import "./Sidebar.css";
 import { Avatar, IconButton } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
-import MoreVertIcon from '@material-ui/icons/MoreVert'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
 
 function Sidebar() {
     return (
+        // BEM Naming Conventions
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar src="https://vignette.wikia.nocookie.net/joke-battles/images/4/40/18360-doge-doge-simple.jpg/revision/latest?cb=20151209161638"/>
@@ -22,8 +24,15 @@ function Sidebar() {
                     </IconButton>
                 </div>
             </div>
+            
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Sidebar
