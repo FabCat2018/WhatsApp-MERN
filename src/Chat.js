b7f20ecb1c732081391c316223f793844782a77b
Fabio Cataleta <fcataleta@gmail.com>
2021-06-15 12:33:09 +0100

    -Added ability to send messages from App -Modified input field styling

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
+import axios from "./axios";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic"
 
 function Chat({ messages }) {
+    const [input, setInput] = useState("");
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        await axios.post("/messages/new", {
+            message: input,
+            name: "Fabio Cataleta",
+            timestamp: "Just now!",
+            received: true
+        });
+
+        setInput("");
+    };
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -42,8 +58,8 @@ function Chat({ messages }) {
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input placeholder="Type a message" type="text" />
-                    <button type="submit">
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage} type="submit">
                         Send a message
                     </button>
                 </form>
