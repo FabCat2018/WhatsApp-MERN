be0860502b4e2c0baa367bef86d2d4a0695dacbb
Fabio Cataleta <fcataleta@gmail.com>
2021-06-15 11:53:01 +0100

    -Imported Pusher and Axios -Added Pusher code to perform real-time updates between the database and the frontend -Added Axios file to allow for retrieval of all previous messages on load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import axios from "./axios";
 
 function App() {
+    const [messages, setMessages] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("/messages/sync").then(response => {
+            setMessages(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        const pusher = new Pusher("750cf084b90b10c2bb4f", {
+            cluster: "eu",
+        });
+        
+        const channel = pusher.subscribe("messages");
+        channel.bind("inserted", (newMessage) => {
+            setMessages([...messages, newMessage]);
+        });
+
+        return () => {
+            channel.unbind_all();
+            channel.unsubscribe();
+        };
+    }, [messages]);
+
+    console.log(messages);
+
     return (
         <div className="app">
             <div className="app__body">
